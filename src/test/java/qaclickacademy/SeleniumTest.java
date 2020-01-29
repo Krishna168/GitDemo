9b1b410e885b21bebdeb0746b1d4c1d7f48add2d
Krishna <krishnahari09@gmail.com>
2020-01-29 21:33:28 +0530

    This is the message of western time zone guy

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -7,6 +7,8 @@ public class SeleniumTest {
 	public void BrowserAutomation()
 	{
 		System.out.println("BrowserAutomation");
+		System.out.println("BrowserAutomation2");
+		System.out.println("BrowserAutomation3");
 	}
 	@Test
 	public void elementsUI()
